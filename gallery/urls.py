08d9c2c3a3466fc41d23ccfd90249b7b475a26a5
toxich <Antoha_1997@mail.ru>
2020-07-04 15:26:23 +0300

    changed views for gallery + local server

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,7 +4,7 @@ from gallery import views
 
 urlpatterns = [
     path(r'gallery', views.gallery, name='gallery'),
-    path(r'gallery', views.start_function),
+    # path(r'gallery', views.start_function),
     path('gallery_function', views.gallery_function, name='gallery_function'),
     path('gallery_checkbox', views.gallery_checkbox, name='gallery_checkbox'),
     path('update_content', views.update_content, name='update_content'),
--- gallery/views.py
+++ gallery/views.py
@@ -6,16 +6,8 @@ from gallery.models import ProductInGallery
 
 
 def gallery(request):
-    typeOfProducts = TypeOfProduct.objects.all()
-    start_function(request)
-    ProductsInGallery = ProductInGallery.objects.all()
-    for product in ProductsInGallery:
-        product.is_active = True
-        product.save(force_update=True)
-    return render(request, 'landing/gallery.html', locals())
-
-
-def start_function(request):
+    type_of_products = TypeOfProduct.objects.all()
+    # start_function(request)
     if not request.session.exists(request.session.session_key):
         request.session.create()
     session_key = request.session.session_key
@@ -23,7 +15,23 @@ def start_function(request):
     for product in products:
         new_product, created = ProductInGallery.objects.get_or_create(session_key=session_key,
             product=product)
-    pass
+    products_in_gallery = ProductInGallery.objects.filter(session_key=session_key)
+    for product in products_in_gallery:
+        product.is_active = True
+        product.save(force_update=True)
+    print(session_key)
+    return render(request, 'landing/gallery.html', locals())
+
+
+# def start_function(request):
+#     if not request.session.exists(request.session.session_key):
+#         request.session.create()
+#     session_key = request.session.session_key
+#     products = Product.objects.all()
+#     for product in products:
+#         new_product, created = ProductInGallery.objects.get_or_create(session_key=session_key,
+#             product=product)
+#     pass
 
 
 def gallery_function(request):
@@ -33,8 +41,9 @@ def gallery_function(request):
     search = data.get("search")
     cancel = data.get("refresh")
     if (cancel=='False'):
-        products = ProductInGallery.objects.filter(Q(product__name__icontains=search, is_active=True) |
-                                                 Q(product__description__icontains=search, is_active=True))[0:12]
+        products = ProductInGallery.objects.filter(
+            Q(session_key=session_key, product__name__icontains=search, is_active=True) |
+                Q(session_key=session_key, product__description__icontains=search, is_active=True))[0:12]
     else:
         products = ProductInGallery.objects.all()[0:12]
     return_dict['data'] = data.get("search")
@@ -55,22 +64,22 @@ def gallery_checkbox(request):
     status = data.get("status")
     if status == 'false':
         filtering_product = ProductInGallery.objects.filter(
-            Q(product__name__icontains=search, product__type__name=checkbox) |
-                Q(product__description__icontains=search, product__type__name=checkbox))
+            Q(session_key=session_key, product__name__icontains=search, product__type__name=checkbox) |
+                Q(session_key=session_key, product__description__icontains=search, product__type__name=checkbox))
         for item in filtering_product:
             item.is_active = False
             item.save(force_update=True)
     else:
         filtering_product = ProductInGallery.objects.filter(
-            Q(product__name__icontains=search, product__type__name=checkbox) |
-            Q(product__description__icontains=search, product__type__name=checkbox))
+            Q(session_key=session_key, product__name__icontains=search, product__type__name=checkbox) |
+            Q(session_key=session_key, product__description__icontains=search, product__type__name=checkbox))
         for item in filtering_product:
             item.is_active = True
             item.save(force_update=True)
 
     result_products = ProductInGallery.objects.filter(
-        Q(product__name__icontains=search, is_active=True) |
-        Q(product__description__icontains=search, is_active=True))[0:12]
+        Q(session_key=session_key, product__name__icontains=search, is_active=True) |
+        Q(session_key=session_key, product__description__icontains=search, is_active=True))[0:12]
     for item in result_products:
         return_dict[item.id] = [item.product.name, item.product.description, item.product.cost,
                                 '/media/' + str(item.product.productimage_set.all()[0]), item.id]
@@ -83,8 +92,9 @@ def update_content(request):
     data = request.POST
     start = int(data.get("quantity"))
     search = data.get("search")
-    products_on_page = ProductInGallery.objects.filter(Q(product__name__icontains=search, is_active=True) |
-                                       Q(product__description__icontains=search, is_active=True))[start-1:start+9]
+    products_on_page = ProductInGallery.objects.filter(
+        Q(session_key=session_key, product__name__icontains=search, is_active=True) |
+           Q(session_key=session_key, product__description__icontains=search, is_active=True))[start-1:start+9]
     for item in products_on_page:
         return_dict[item.id] = [item.product.name, item.product.description, item.product.cost,
                                 '/media/' + str(item.product.productimage_set.all()[0]), item.id]
