4c7f200a60e4bb4792b0704052f49b018f53bccf
Anton Gladkih <agladkih@webim-team.ru>
2020-10-18 12:11:59 +0300

    Добавил разделение на local/prod + Dockerfile changed. update-docker

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,66 @@
-FROM python:3.8
+# BUILDER #
+###########
 
-WORKDIR /usr/src/app/
+# pull official base image
+FROM python:3.8.3 as builder
 
+# set work directory
+WORKDIR /usr/src/parfumadmin
+
+# set environment variables
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
+RUN apt-get update
+RUN apt-get upgrade -y && apt-get -y install postgresql gcc python3-dev musl-dev
+# install dependencies
 RUN pip install --upgrade pip
-COPY ./requirements.txt .
-RUN pip install -r requirements.txt
 
-COPY . .
+COPY ./requirements.txt .
+#RUN python -m pip install pip==19.3.1
+RUN pip wheel --no-cache-dir --no-deps --wheel-dir /usr/src/parfumadmin/wheels -r requirements.txt
+
+
+#########
+# FINAL #
+#########
+
+# pull official base image
+FROM python:3.8.3
+
+# create directory for the app user
+RUN mkdir -p /home/parfumadmin
+
+# create the app user
+RUN groupadd parfumeria
+RUN useradd -m -g parfumeria parfumadmin
+RUN usermod -aG parfumeria parfumadmin
+
+# create the appropriate directories
+ENV HOME=/home/parfumadmin
+ENV APP_HOME=/home/parfumadmin/django
+RUN mkdir $APP_HOME
+RUN mkdir $APP_HOME/staticfiles
+RUN mkdir $APP_HOME/media
+
+# create the appropriate directories
+WORKDIR $APP_HOME
+
+RUN apt-get update && apt-get install -y netcat
+# install dependencies
+COPY --from=builder /usr/src/parfumadmin/wheels /wheels
+COPY --from=builder /usr/src/parfumadmin/requirements.txt .
+RUN pip install --no-cache /wheels/*
+
+COPY ./entrypoint.sh $APP_HOME
+
+# copy project
+COPY . $APP_HOME
+
+# chown all the files to the app user
+RUN chown -R parfumadmin:parfumeria $APP_HOME
+
+# change to the app user
+USER parfumadmin
+
+ENTRYPOINT ["/home/parfumadmin/django/entrypoint.sh"]
---
+++ Magazin_site/settings_prod.py
@@ -0,0 +1,100 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = int(os.environ.get("DEBUG", default=0))
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'landing',
+    'products',
+    'gallery'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'Magazin_site.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'landing.context_processors.search_form_in_navbar',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'Magazin_site.wsgi.application'
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
